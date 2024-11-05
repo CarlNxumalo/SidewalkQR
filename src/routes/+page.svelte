392b082233b4b87709ecf89fb5b611eee51c6c59
CarlNxumalo <carl.ybongani@gmail.com>
2024-11-05 22:24:19 +0200

    Padding section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
   
 <Section name="crudreadsection" class="flex justify-center items-center mt-16 pt-8"> <!-- Add mt-16 here -->
-    <div class="p-6 space-y-4 md:space-y-6 sm:p-8 max-w-md mx-auto">
+    <div class="p-6 space-y-4 md:space-y-6 sm:p-8 max-w-md mx-auto mx-auto mt-12">
         <Card img="SideWalkSpecial.jpg">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Noteworthy technology acquisitions 2021

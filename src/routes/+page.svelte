b2ace94d81e3d3ba653922d8e02cb0deef1d78c3
CarlNxumalo <carl.ybongani@gmail.com>
2024-11-05 21:37:08 +0200

    Woriking one

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Card, Button, Toggle } from 'flowbite-svelte';
+    import {Section} from 'flowbite-svelte-blocks'
+    import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+  
+  <Section name="crudreadsection" class="flex justify-center items-center">
+    <div class="p-6 space-y-4 md:space-y-6 sm:p-8 max-w-md mx-auto">
+        <Card img="SideWalkSpecial.jpg">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          </p>
+          <Button>
+            Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+          </Button>
+        </Card>
+    </div>
+</Section>

34dd5840f57d028ec27acc5cc2d72fd8296ba52e
CarlNxumalo <carl.ybongani@gmail.com>
2024-11-05 22:11:00 +0200

    navbarr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import { ArrowRightOutline } from 'flowbite-svelte-icons';
 </script>
   
-  <Section name="crudreadsection" class="flex justify-center items-center">
+<Section name="crudreadsection" class="flex justify-center items-center mt-16 pt-8"> <!-- Add mt-16 here -->
     <div class="p-6 space-y-4 md:space-y-6 sm:p-8 max-w-md mx-auto">
         <Card img="SideWalkSpecial.jpg">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">

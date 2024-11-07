aa22ca7924c9783cf9bc46d0bd3a5ff7bb011c95
CarlNxumalo <carl.ybongani@gmail.com>
2024-11-07 17:05:16 +0200

    working stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,20 @@
 </script>
   
 <div class="relative px-8">
-	<Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b h-16">
+	<Navbar class="px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 start-0 border-b h-16 ">
 	  <NavBrand href="/">
 		<!--=<img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
 	  </NavBrand>
 	  <NavHamburger />
 	  <NavUl>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/about">We</NavLi>
-		<NavLi href="/docs/components/navbar">Navbar</NavLi>
-		<NavLi href="/pricing">Pricing</NavLi>
-		<NavLi href="/contact">Contact</NavLi>
+		<NavLi href="/monday">Monday</NavLi>
+		<NavLi href="/about">Tuesday</NavLi>
+		<NavLi href="/docs/components/navbar">Wednesday</NavLi>
+		<NavLi href="/pricing">Thursday</NavLi>
+		<NavLi href="/contact">Friday</NavLi>
+		<NavLi href="/contact">Saturday</NavLi>
+		<NavLi href="/contact">Sunday</NavLi>
 	  </NavUl>
 	</Navbar>
 </div>
---
+++ src/routes/+page.server.js
@@ -0,0 +1,17 @@
+import { error } from '@sveltejs/kit';
+export function load({ params }) {
+    
+    const dailyImages = {
+        0: "SundaySpecial.jpg",    // Sunday
+        1: "MondaySpecial.jpg",    // Monday
+        2: "TuesdaySpecial.jpg",   // Tuesday
+        3: "WednesdaySpecial.jpg", // Wednesday
+        4: "ThursdaySpecial.jpg",  // Thursday
+        5: "FridaySpecial.jpg",    // Friday
+        6: "SaturdaySpecial.jpg"   // Saturday
+      };
+    
+      // Get the current day of the week (0 = Sunday, 6 = Saturday)
+      const today = new Date().getDay();
+      const todayImage = dailyImages[today];
+}
